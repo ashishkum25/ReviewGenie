6cc0d80c9d83fc7cc6786338e81040890d3dc4de
Ashish Kumar <ashishkum0807@gmail.com>
2025-02-14 01:49:05 +0530

    UI Completed

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const aiRoute = require('./routes/ai.route.js');
 const app = express()
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json())
 
 app.use('/ai', aiRoute);
